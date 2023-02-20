fc4d9ad8aa5374d1b3076062f48fe9a73c99f15e
King2oo9 <120119639+King2oo9@users.noreply.github.com>
2023-02-20 13:06:22 +0500

    host

--- kutubxona/kutubxona/settings.py
+++ kutubxona/kutubxona/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_@=5mhj5*s0=hwmo0q%&og97d6xf63$6(vzj-4)!zprh-c+lm2'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["https://libaryuzb.netlify.app/"]
 
 # Application definition
 
